43a653cae6039cc5865a19e979985a2f573a47a8
togana <o.togana@gmail.com>
2022-12-10 13:41:45 +0900

    placeholder の style 調整

--- src/components/lexical/styled/placeholder.tsx
+++ src/components/lexical/styled/placeholder.tsx
@@ -1,9 +1,10 @@
 import { styled } from "@stitches/react";
 
 export const Placeholder = styled('div', {
-  'position': 'absolute',
-  'top': '0.5rem',
-  'left': '0.5rem',
+  color: 'gray',
+  position: 'absolute',
+  top: '0.5rem',
+  left: '0.5rem',
   pointerEvents: 'none',
   userSelect: 'none',
 });
